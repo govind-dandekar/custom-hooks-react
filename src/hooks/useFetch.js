56766d6bf25502ef05c54acaaa14f64da8294a05
Govind Dandekar <govind@Govinds-Mini.lan>
2024-12-10 08:40:07 -0800

    implement useFetch hook in App

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,36 @@
+import { useEffect, useState } from 'react';
+
+// fx that start with "use" are treated as hooks by React
+export function useFetch(fetchFn, initialValue){
+
+  const [isFetching, setIsFetching] = useState();
+  const [error, setError] = useState();
+  const [fetchedData, setFetchedData] = useState(initialValue);
+
+  // can use other hooks inside fx
+	useEffect(() => {
+    async function fetchData() {
+      setIsFetching(true);
+      try {
+        const data = await fetchFn();
+        setFetchedData(data);
+      } catch (error) {
+        setError({ message: error.message || 'Failed to fetch data.' });
+      }
+
+      setIsFetching(false);
+    }
+
+    fetchData();
+  }, 
+  // if fetchFn fx changes, hook should run again 
+  // with updated fetchFn fx
+  [fetchFn]);
+
+  // return state by grouping it into an array or obj
+  return {
+    isFetching,
+    fetchedData,
+    error
+  }
+}
